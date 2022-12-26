4ba8afdeb0bd1f3928df15bd1ca7f0cea7f26b3c
MaxiSound <maxisound@yandex.ru>
2022-12-26 08:51:35 +0700

    ready

--- Wardrobe/Interaction.java
+++ Wardrobe/Interaction.java
@@ -1,5 +1,35 @@
 package Wardrobe;
 
+import java.util.ArrayList;
+
 public class Interaction {
-    
+    public boolean tryToOpen (boolean open,boolean keys){
+        if (open || keys){
+            System.out.println("Шкаф открылся");
+            return true;
+
+        }else {
+            System.out.println("Шкаф закрыт");
+            return false;
+        }
+    }
+        public void travel(boolean roadToNarnia, boolean wishToTravel) {
+        if (roadToNarnia && wishToTravel) {
+            System.out.println("Добро пожаловать");
+        } else {
+            System.out.println("Не получиться");
+        }
+    }
+
+    public ArrayList<String> wearing(String[] contain, String[] wear) {
+        ArrayList result = new ArrayList();
+        for (String s : contain) {
+            result.add(s);
+        }
+        for (String s : wear) {
+            result.add(s);
+        }
+        return  result;
+    }
+
 }
