59a57bc02b4eacfbe29738d189dd4d5e59e44091
MaxiSound <maxisound@yandex.ru>
2022-12-26 08:56:16 +0700

    ready

--- Wardrobe/Human.java
+++ Wardrobe/Human.java
@@ -1,5 +1,15 @@
 package Wardrobe;
 
+import java.util.ArrayList;
+
 public class Human {
-    
+    String [] cloth;
+    Boolean keys;
+    Boolean wishToTravel;
+
+        Human(boolean keys, boolean wishToTravel, String[] cloth){
+        this.cloth = cloth;
+        this.keys =keys;
+        this.wishToTravel= wishToTravel;
+    }
 }
