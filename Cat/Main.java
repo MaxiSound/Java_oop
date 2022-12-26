527d745b7a130b5ddca0740aafe95a0f7a6131c2
MaxiSound <maxisound@yandex.ru>
2022-12-26 09:23:00 +0700

    ready

--- Cat/Main.java
+++ Cat/Main.java
@@ -1,5 +1,33 @@
 package Cat;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        /*
+        Human human  = new Human();
+        human.setAtHome(true);
+        human.setWishToHug(true);
+        System.out.println("Поговорим про кота.");
+        Cat cat = new Cat();
+        cat.setName("Киллер");
+        cat.setHungry(true);
+        cat.setSleep(false);
+        cat.setWishToHug(true);
+        human.feedCat(cat.getName(), human.atHome,cat.hungry,cat.sleep);
+        cat.feed();
+        human.hugCat(cat.getName(),human.atHome,cat.wishToHug,human.wishToHug,cat.sleep);
+        cat.play();
+        System.out.println("Теперь поговорим про волчару.");
+        Wolf wolf = new Wolf();
+        wolf.setName("Серый");
+        wolf.hide();
+        wolf.bite();
+                 */
+        Cat cat = new Cat();
+        ArrayList <Animal> group = cat.createGroup(20);
+        Set<Animal> uniq = cat.showUniqHash(group);
+        System.out.println(cat.showUniqCat(uniq));
+        }
 }
