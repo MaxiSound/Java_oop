ddd6a992a63b5fe075f49a891a02b51f3f98de9d
MaxiSound <maxisound@yandex.ru>
2022-12-26 08:39:53 +0700

    ready

--- Wardrobe/Main.java
+++ Wardrobe/Main.java
@@ -1,5 +1,10 @@
 package Wardrobe;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Wardrobe wardrobe = new Wardrobe();
+
+        Logic logic = new Logic();
+        logic.game();
+    }
 }
