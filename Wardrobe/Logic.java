833ac9f129d0ea3d07bcf4a171ad096dff2f51df
MaxiSound <maxisound@yandex.ru>
2022-12-26 08:47:43 +0700

    ready

--- Wardrobe/Logic.java
+++ Wardrobe/Logic.java
@@ -1,5 +1,18 @@
 package Wardrobe;
 
 public class Logic {
-    
+    Human human = new Human(true, true, new String[]{"Hat"});
+    Wardrobe wardrobe = new Wardrobe("wood", false, true, new String[]{"Pants", "Shoes", "Shirt"});
+    Interaction interaction = new Interaction();
+    Logic () {}
+
+    public void game() {
+        wardrobe.repairMeNow();
+        if (interaction.tryToOpen(human.keys, wardrobe.open)) {
+            System.out.println("Твоя одежда: " + interaction.wearing(human.cloth, wardrobe.contain));
+            interaction.travel(human.wishToTravel, wardrobe.roadToNarnia);
+        } else {
+            System.out.println("Попробуй в следующий раз");
+        }
+    }
 }
