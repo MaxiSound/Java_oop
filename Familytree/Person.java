00f483e2e31c0bc7b30cd413ae70bb287f70bb2b
MaxiSound <maxisound@yandex.ru>
2022-12-26 10:18:42 +0700

    ready

--- Familytree/Person.java
+++ Familytree/Person.java
@@ -1,5 +1,13 @@
 package Familytree;
 
 public class Person {
-    
+    public String fullName;
+
+    public Person(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
 }
