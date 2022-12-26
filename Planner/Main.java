0c1e7bfeea691b2823a84223a706645eb99fe868
MaxiSound <maxisound@yandex.ru>
2022-12-26 11:03:06 +0700

    ready

--- Planner/Main.java
+++ Planner/Main.java
@@ -1,5 +1,14 @@
 package Planner;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException, ParseException {
+        Notebook notebook = new Notebook();
+        notebook.createName();
+        notebook.fillSheet();
+        notebook.outputToCsvFile(notebook.getName(), notebook.getSheet());
+        notebook.showPriority("Test");
+    }
 }
