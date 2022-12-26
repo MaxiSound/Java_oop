23f53b02dd51bad4113acad3d49729737d9747fa
MaxiSound <maxisound@yandex.ru>
2022-12-26 09:33:51 +0700

    ready

--- Cat/Wolf.java
+++ Cat/Wolf.java
@@ -4,5 +4,27 @@ import java.util.ArrayList;
 import java.util.Set;
 
 public class Wolf extends Animal implements Wild {
-    
+    public Wolf(String kind, int age, int weight) {
+        super(kind, age, weight);
+    }
+
+    @Override
+    public void hide() {
+        System.out.println(name + " прячется");
+    }
+
+    @Override
+    public void bite() {
+        System.out.println(name + " кусается");
+    }
+
+    @Override
+    public ArrayList<Animal> createGroup(int size) {
+        return null;
+    }
+
+    @Override
+    public Set<Animal> showUniqHash(ArrayList<Animal> a) {
+        return null;
+    }
 }
