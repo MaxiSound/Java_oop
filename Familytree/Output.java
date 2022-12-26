77cddda8492b7ca8c41480bcd134b887d006400d
MaxiSound <maxisound@yandex.ru>
2022-12-26 09:48:33 +0700

    ready

--- Familytree/Output.java
+++ Familytree/Output.java
@@ -1,5 +1,8 @@
 package Familytree;
 
-public class Output {
-    
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface Output {
+    void outputToFile(String filename, ArrayList n) throws IOException;
 }
