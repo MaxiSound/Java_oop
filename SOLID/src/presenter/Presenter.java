01af075f0c331fc9be9ffb33853dc1698ad1352d
MaxiSound <maxisound@yandex.ru>
2023-02-02 15:08:42 +0700

    go

---
+++ SOLID/src/presenter/Presenter.java
@@ -0,0 +1,13 @@
+package presenter;
+import model.Number;
+import model.Output;
+import view.View;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Presenter {
+    
+}
